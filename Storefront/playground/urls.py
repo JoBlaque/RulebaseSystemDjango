e2074e4339df22307c958fc7f01e32d1b8281a06
Faraz Khalil <mughalfrazk@gmail.com>
2024-07-25 08:02:50 +0100

    happy flow for demo

--- Storefront/playground/urls.py
+++ Storefront/playground/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 #UrlConfig
 urlpatterns =[
     path('hello/', views.say_hello),
+    path("planner_api/", views.planner_api),
 ]
--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from RulebaseProductionSystem import planner
+from django.http import JsonResponse
+
 
 # Create your views here.
 # request -> response
@@ -10,7 +14,14 @@ def say_hello(request):
     # pull data from db
     # transform data 
     # send email
+    return render(request, 'new.html')
 
 
+@api_view(['POST'])
+def planner_api(request):
+    plan = planner.planner(initial_state=request.data.get('initial_state'), final_state=request.data.get('final_state'),
+                           positions={"type": "discrete", "values": request.data.get('position_value')},
+                           constraints=request.data.get('constraints'), facts=request.data.get('general_fact'),
+                           iteration_limit=1000)
 
-    return render(request, 'hello.html')
+    return JsonResponse({"plan": plan})
